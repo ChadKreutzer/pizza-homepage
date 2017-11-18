cfc09e0c2df6069f9d161f359919dce4a355f550
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-18 17:44:22 -0600

    generates homepage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const menuContainer = buildTabs();
 const contentContainer = buildContentContainer();
+const pizzaText = "Our pizza is the best pizza to ever grace the earth. We hand make all pizzas and ingrediants. We manage to hand craft pizzas that are wonderful, authentic, and still have a low cost. If possible our ingrediants are locally sourced and bought straight from the farmers themselves. Try one today!"
 
 function buildTabs() {
   let menuContainer = document.createElement("div");
@@ -21,4 +22,43 @@ function buildContentContainer() {
   contentContainer.id = "content";
   document.body.appendChild(contentContainer);
   return contentContainer;
+}
+
+function clearContent(){
+console.log("CLEAR");
+}
+
+function generateHomePage() {
+  placePizza();
+  placeText();
+
+  function placePizza() {
+    let pizzaImage = document.createElement("img");
+    pizzaImage.setAttribute("src", "http://4.bp.blogspot.com/_21d4pBhy3P4/RYRZ_ba8umI/AAAAAAAAADg/3Ri9X433shM/s320/pizzaclock-4.jpg");
+    contentContainer.appendChild(pizzaImage)
+  }
+
+  function placeText() {
+    let textContainer = document.createElement("div")
+    textContainer.classList.add("Text-Content");
+
+    let headerText = document.createElement("h3");
+    headerText.textContent = "Time for Pizza!";
+
+    let paragraphText = document.createElement("p");
+    paragraphText.textContent = pizzaText;
+
+    textContainer.appendChild(headerText);
+    textContainer.appendChild(paragraphText);
+
+    contentContainer.appendChild(textContainer);
+  }
+}
+
+function generateMenuPage() {
+
+}
+
+function generateContactPage() {
+
 }
