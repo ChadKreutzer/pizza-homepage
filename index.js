e1badb69648296cdb9ff203eba2270cb2bdf2b8a
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-23 20:59:21 -0600

    Refactor tab event listeners

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const tabContainer = buildTabs();
-const contentContainer = buildContentContainer();
+const tabContentContainer = buildContentContainer();
 const pizzaText = "Our pizza is the best pizza to ever grace the earth. We hand make all pizzas and ingredients. We manage to hand craft pizzas that are wonderful, authentic, and still have a low cost. If possible our ingredients are locally sourced and bought straight from the farmers themselves. Try one today!";
 const pizzaImageSrc = "http://www.foodanddine.com/wp-content/uploads/2016/05/Pizza-capricciosa.jpg";
+const tabContainer = buildTabs();
 
-(function buildWebPage() {
-  document.body.appendChild(tabContainer);
-  tabContainer.firstChild.classList.add("clicked");
-  document.body.appendChild(contentContainer);
-})();
-  showPage("Home");
+document.body.appendChild(tabContainer);
+tabContainer.firstChild.classList.add("clicked");
+document.body.appendChild(tabContentContainer);
+showPage("Home");
+
+Array.from(tabContainer.children).forEach(function(tab) {
+  tab.addEventListener("click", function() {
+    showPage(tab.textContent);
+    clearTabs();
+    tab.classList.add("clicked");
+  })
+});
 
 function buildTabs() {
   const tabContainer = document.createElement("div");
@@ -23,18 +29,10 @@ function buildTabs() {
   return tabContainer;
 }
 
-Array.from(tabContainer.children).forEach(function(tab) {
-  tab.addEventListener("click", function() {
-    showPage(tab.textContent);
-    clearTabs();
-    tab.classList.add("clicked");
-  })
-});
-
 function buildContentContainer() {
-  const contentContainer = document.createElement("div");
-  contentContainer.id = "content";
-  return contentContainer;
+  const tabContentContainer = document.createElement("div");
+  tabContentContainer.id = "content";
+  return tabContentContainer;
 }
 
 
@@ -50,7 +48,7 @@ function clearTabs() {
 
 function generateHomePage(parentContainer) {
   const pizzaImage = generateImage(pizzaImageSrc);
-  const pizzaTextBlock = generatePizzaText(pizzaText);
+  const pizzaTextBlock = generatePizzaText();
 
   return [pizzaImage, pizzaTextBlock];
 
@@ -126,8 +124,8 @@ function generateContactPage() {
 }
 
 function showPage(tabName) {
-  clearContent(contentContainer);
-  appendToPage(contentContainer, selectGenerator(tabName));
+  clearContent(tabContentContainer);
+  appendToPage(tabContentContainer, selectGenerator(tabName));
 }
 
 function selectGenerator(tabName) {
